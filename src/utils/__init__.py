3965f57515262bec0d454d28fa237b20b2c6d39c
witra <tahjudil.witra@gmail.com>
2025-08-08 16:43:20 +0700

    add copied date, correct the directory

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,10 @@
 # This directory includes source code copied from the Copernicus-FM project:
 # https://github.com/zhu-xlab/Copernicus-FM
+# copied on 2025-08-08
 #
 # License: Apache License 2.0
 #
-# All files under this directory (including subdirectories `aurora/`, `Flexit/`, `XXXXX/`, etc.)
+# All files under this directory (including subdirectories `aurora/`, `Flexivit/`, `utils/`, etc.)
 # were copied from the original repository, with minimal or no changes unless otherwise noted
 # in the individual file.
 #
