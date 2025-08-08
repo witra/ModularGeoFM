a4f67f5a870fb85c6e26cf86100cd3b9b8094775
witra <tahjudil.witra@gmail.com>
2025-08-08 16:32:51 +0700

    add attribution for copernicus-fm package

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+# This directory includes source code copied from the Copernicus-FM project:
+# https://github.com/zhu-xlab/Copernicus-FM
+#
+# License: Apache License 2.0
+#
+# All files under this directory (including subdirectories `aurora/`, `Flexit/`, `XXXXX/`, etc.)
+# were copied from the original repository, with minimal or no changes unless otherwise noted
+# in the individual file.
+#
+# You may obtain a copy of the license at:
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Refer to the original repository for additional notices or third-party attributions.
