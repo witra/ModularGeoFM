f0ecf54145085c678ba5f93714646c2ec5e1b364
witra <tahjudil.witra@gmail.com>
2025-08-08 10:28:29 +0700

    add attribution

--- src/backbones/decoder/satmae_decoder.py
+++ src/backbones/decoder/satmae_decoder.py
@@ -1,23 +1,31 @@
 import torch
 import torch.nn as nn
-from timm.models.vision_transformer import Block, PatchEmbed
+from timm.models.vision_transformer import Block
 
-"""
-Ref: https://github.com/IBM/terratorch/blob/main/terratorch/models/decoders/satmae_head.py
-"""
+
 class SatMAEHeadViT(nn.Module):
+    """
+    Copied from IBM TerraTorch:
+    https://github.com/IBM/terratorch/blob/main/terratorch/models/decoders/satmae_head.py
+    Commit: 4a4b70e (copied on 2025-08-07)
 
-    def __init__(self, embed_dim:int=None, out_channels:int=None, bias:bool=True,
-            num_heads:int=None, mlp_ratio:float=None, patch_size:int=None, num_patches:int=1,
-                 depth:int=None, norm_layer=nn.LayerNorm, in_chans:int=None) -> None:
+    License: Apache License 2.0
+    Copyright (c) IBM Corp.
+
+    Included here under the terms of the Apache 2.0 license.
+    You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+    See LICENSES/APACHE-2.0.txt for the full license text.
+    """
+    def __init__(self, embed_dim: int = None, out_channels: int = None, bias: bool = True,
+                 num_heads: int = None, mlp_ratio: float = None, patch_size: int = None, num_patches: int = 1,
+                 depth: int = None, norm_layer=nn.LayerNorm, in_chans: int = None) -> None:
 
         super(SatMAEHeadViT, self).__init__()
 
-        if type(embed_dim)==tuple:
+        if type(embed_dim) == tuple:
             self.embed_dim = embed_dim[1]
         else:
             self.embed_dim = embed_dim
-
 
         self.out_channels = out_channels
         self.bias = bias
@@ -33,11 +41,14 @@ class SatMAEHeadViT(nn.Module):
 
         self.mask_token = nn.Parameter(torch.zeros(1, 1, self.out_channels))
 
-        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, self.num_patches + 1, self.out_channels), requires_grad=False)  # fixed sin-cos embedding
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, self.num_patches + 1, self.out_channels),
+                                              requires_grad=False)  # fixed sin-cos embedding
 
         self.decoder_blocks = nn.ModuleList([
-            Block(self.out_channels, self.num_heads, self.mlp_ratio, qkv_bias=True, qk_norm=None, norm_layer=self.norm_layer)
+            Block(self.out_channels, self.num_heads, self.mlp_ratio, qkv_bias=True, qk_norm=None,
+                  norm_layer=self.norm_layer)
             for i in range(self.depth)])
+
         self.decoder_norm = self.norm_layer(self.out_channels)
         self.decoder_pred = nn.Linear(self.out_channels, self.patch_size ** 2 * self.in_chans,
                                       bias=True)  # decoder to patch
@@ -61,19 +72,19 @@ class SatMAEHeadViT(nn.Module):
 
     def forward(self, x) -> torch.Tensor:
 
-        # x = torch.stack(x, dim=0)
+        x = torch.stack(x, dim=0)
 
         # embed tokens
         x = self.decoder_embed(x)
 
         # append mask tokens to sequence
-        #mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
-        #x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
-        #x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
-        #x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+        # mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        # x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        # x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        # x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
 
         # add pos embed
-        #x = x + self.decoder_pos_embed
+        # x = x + self.decoder_pos_embed
 
         # apply Transformer blocks
         for blk in self.decoder_blocks:
