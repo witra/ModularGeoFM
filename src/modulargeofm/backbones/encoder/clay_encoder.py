da93c4ffe86b2e74cfa2769066582968024c583d
witra <tahjudil.witra@gmail.com>
2026-02-19 12:46:15 +0100

    clay basis encoder

--- src/modulargeofm/backbones/encoder/clay_encoder.py
+++ src/modulargeofm/backbones/encoder/clay_encoder.py
@@ -3,11 +3,11 @@
 # Licensed under the Apache License 2.0.
 #
 # Included classes:
-# - `SegmentEncoder()`: Adapted from `claymodel/finetune/segment/factory.py`
+# - `ClayTransformerEncoder()`: insipred from `claymodel/finetune/segment/factory.py`
 #   Source: https://github.com/Clay-foundation/model/blob/main/claymodel/finetune/segment/factory.py
 #
 # Modifications:
-# - Combined both classes into a single file
+# - implemented SegmentEncoder to ClayTransformerEncoder
 # - Adjusted import paths and minor formatting
 #
 # You may obtain a copy of the Apache License 2.0 at:
@@ -129,6 +129,116 @@ class SegmentEncoder(ClayEncoder):
         return patches
 
 
+
+class ClayTransformerEncoder(ClayEncoder):
+    """
+    Core transformer-based encoder derived from the Clay foundation model.
+
+    It serves as a reusable feature extractor for downstream tasks 
+    like segmentation, and regression.
+    
+    Reference: https://github.com/Clay-foundation/model/blob/main/claymodel/finetune/segment/factory.py
+    """
+    def __init__(  # noqa: PLR0913
+        self,
+        mask_ratio,
+        patch_size,
+        shuffle,
+        dim,
+        depth,
+        heads,
+        dim_head,
+        mlp_ratio,
+        ckpt_path=None,
+    ):
+        super().__init__(
+            mask_ratio,
+            patch_size,
+            shuffle,
+            dim,
+            depth,
+            heads,
+            dim_head,
+            mlp_ratio,
+        )
+
+        # Set device
+        self.device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+        # Load model from checkpoint if provided
+        self.load_from_ckpt(ckpt_path)
+    
+    def load_from_ckpt(self, ckpt_path):
+        """
+        Load the model's state from a checkpoint file.
+
+        Args:
+            ckpt_path (str): The path to the checkpoint file.
+        
+        """
+        if ckpt_path:
+            # Load checkpoint
+            ckpt = torch.load(ckpt_path, map_location=self.device)
+            state_dict = ckpt.get("state_dict")
+
+            # Prepare new state dict with the desired subset and naming
+            new_state_dict = {
+                re.sub(r"^model\.encoder\.", "", name): param
+                for name, param in state_dict.items()
+                if name.startswith("model.encoder")
+            }
+
+            # Load the modified state dict into the model
+            model_state_dict = self.state_dict()
+            for name, param in new_state_dict.items():
+                if (
+                    name in model_state_dict
+                    and param.size() == model_state_dict[name].size()
+                ):
+                    model_state_dict[name].copy_(param)
+                else:
+                    print(f"No matching parameter for {name} with size {param.size()}")
+
+            # Freeze the loaded parameters
+            for name, param in self.named_parameters():
+                if name in new_state_dict:
+                    param.requires_grad = False
+
+    def forward(self, datacube):
+        """
+        Forward pass of the SegmentEncoder.
+
+        Args:
+            datacube (dict): A dictionary containing the input datacube and
+                meta information like time, latlon, gsd & wavelenths.
+
+        Returns:
+            list: A list of feature maps extracted from the datacube.
+        
+        """
+        cube, time, latlon, gsd, waves = (
+            datacube["pixels"],  # [B C H W]
+            datacube["time"],  # [B 2]
+            datacube["latlon"],  # [B 2]
+            datacube["gsd"],  # 1
+            datacube["waves"],  # [N]
+        )
+
+        B, C, H, W = cube.shape
+
+        # Patchify and create embeddings per patch
+        patches, waves_encoded = self.to_patch_embed(cube, waves)  # [B L D]
+        patches = self.add_encodings(patches, time, latlon, gsd)  # [B L D]
+
+        # Add class tokens
+        cls_tokens = repeat(self.cls_token, "1 1 D -> B 1 D", B=B)  # [B 1 D]
+        patches = torch.cat((cls_tokens, patches), dim=1)  # [B (1 + L) D]
+
+        patches = self.transformer(patches)
+        patches = patches[:, 1:, :]  # [B L D] , D = dim
+
+        return patches
 def clay_encoder_small(**kwargs):
     args = {
         # ENCODER
